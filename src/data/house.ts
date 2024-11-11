24a6ce9d68e4d0da74a43fbab104822a889597fa
DUSHIME1212 <mdonavan33@gmail.com>
2024-11-11 20:42:03 +0200

    2

--- src/data/house.ts
+++ src/data/house.ts
@@ -8,7 +8,7 @@ export const house = [
           "https://i.pinimg.com/564x/1a/2a/cf/1a2acf17bff4a14008607667cfaccd4f.jpg",
         title: "  For Rent - 3 Bedroom House",
         subtitle: "  Gasabo-Remera-Amahoro village",
-        price: "rwf 1,500,000",
+        price: "rwf 1,500,000 ",
       },
       {
         id:2,
