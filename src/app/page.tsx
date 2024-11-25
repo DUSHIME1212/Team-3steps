401841147767a864d3bede9b01b8086a268b1ac7
honorine22 <higiranez@gmail.com>
2024-11-25 16:39:43 +0200

    remove navbar and footer from auth pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import About from "@/components/About";
+import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import Listings from "@/components/Listings";
+import Navbar from "@/components/Navbar";
 import Testinmonial from "@/components/Testinmonial";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
@@ -9,11 +11,15 @@ import 'react-toastify/dist/ReactToastify.css';
 export default function Home() {
   return (
     <main >
+      <header>
+        <Navbar />
+      </header>
       <ToastContainer position="top-right" />
       <Hero />
       <About />
       <Listings />
       <Testinmonial />
+      <Footer />
     </main>
   );
 }
